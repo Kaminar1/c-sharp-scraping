5770ab2c4d75774a5941f70bc7b2b1650e320f5a
Kaminari <4765250+Kaminar1@users.noreply.github.com>
2023-01-20 15:39:04 +0100

    puppeteersharp test

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
 ﻿
 //missing some of the font scripts. kind of presentable if fixing font, and relative paths.
-using HtmlAgilityPack;
+// using HtmlAgilityPack;
 
-HtmlWeb web = new HtmlWeb();
-HtmlDocument doc = web.Load("https://norgesnett.no/aktuelt/elavgiften-er-lavere-i-januar-februar-og-mars-2023/");
+// HtmlWeb web = new HtmlWeb();
+// HtmlDocument doc = web.Load("https://norgesnett.no/aktuelt/elavgiften-er-lavere-i-januar-februar-og-mars-2023/");
 
-var content = doc.DocumentNode.InnerHtml;
+// var content = doc.DocumentNode.InnerHtml;
 
-await File.WriteAllTextAsync("HtmlAgilityPack.html", content);
 
+// await File.WriteAllTextAsync("output/HtmlAgilityPack.html", content);
+
+
+using PuppeteerSharp;
+
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+{
+    Headless = true
+});
+
+using (var page = await browser.NewPageAsync())
+{
+    await page.GoToAsync("https://norgesnett.no/aktuelt/elavgiften-er-lavere-i-januar-februar-og-mars-2023/");
+    
+    await page.WaitForSelectorAsync("#x-site > header");
+    
+    var content = await page.GetContentAsync();
+
+    await File.WriteAllTextAsync("output/PuppeteerSharp.html", content);
+}
+
+
+//cera-round-pro, "Helvetica Neue", "Helvetica", "Arial", sans-serif
